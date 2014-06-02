2c72f6ec986bf623e4d437ef3b2405b61c4cedcb
CiaranR <ciaran.rooney@gmail.com>
2014-06-02 01:57:18 +0100

    first draft of widgets
    basic date, key pair, string and number using slim and composer

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+  require 'vendor/autoload.php';
+  
+  $widgets = new \Slim\Slim();
+  
+  $widgets->response->headers->set('Content-Type', 'application/json');
+
+  $widgets->get('/text/:text', function ($text) {
+	$result = new stdClass();	
+	$result->item = [["text" => $text]];
+	echo json_encode($result);
+  });
+  
+  $widgets->get('/number/:number', function ($number) {
+	$result = new stdClass();	
+	$result->item = [["value" => $number]];	
+	echo json_encode($result);
+  })->conditions(array('number' => '[0-9]{1,}'));
+  
+  $widgets->get('/keypair/:text/:number', function ($text, $number) {
+	$result = new stdClass();	
+	$result->item = [["value" => $number, "text" => $text]];	
+	echo json_encode($result);
+  });
+  
+  $widgets->get('/countdown/:date', function ($date) {
+	$now = new DateTime('now');
+	$date2 = new DateTime($date);
+	$interval = $now->diff($date2);
+	$result = new stdClass();	
+	$result->item = [["value" => $interval->format('%r%a'), "text" => "days until ".$date]];	
+	echo json_encode($result);
+  });
+  
+  
+  $widgets->run();
